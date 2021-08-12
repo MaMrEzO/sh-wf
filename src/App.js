e6da7119f6b2be7e86dd1142a47bca3ef39588d2
nima-dvlp <nima85shz@gmail.com>
2021-08-12 15:35:34 +0430

    Initiate with try to load localStorage

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import {WorkFlowBox} from './components/WorkFlow';
 import {WorkflowData, State} from './types/Types';
 
 var sample_store = new WorkflowData();
+sample_store.restore();
 
-sample_store.add("To do", null, false, React.createRef());
-sample_store.add("Make project", 1, false, React.createRef());
-sample_store.add("Make repo on git", 1, true, React.createRef());
-sample_store.add("Init git", 3, false, React.createRef());
+//sample_store.add("To do", null, false, React.createRef());
+//sample_store.add("Make project", 1, false, React.createRef());
+//sample_store.add("Make repo on git", 1, true, React.createRef());
+//sample_store.add("Init git", 3, false, React.createRef());
 
 
 //console.log("Tree", sample_store, sample_store.tree);
